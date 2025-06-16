e70f253a91d238eb11c8bf837871fc80c15c6a2b
YUppi1113 <120902617+YUppi1113@users.noreply.github.com>
2025-06-17 05:33:21 +0900

    Setup Tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html', './js/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#667eea',
+          dark: '#764ba2'
+        }
+      },
+      fontFamily: {
+        sans: ['Noto Sans JP', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
